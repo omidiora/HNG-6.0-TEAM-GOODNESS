32f9e7f9a3ed679878043c0eb6b5beab82f5264f
Omidiora Emmanuel <45630269+omidiora@users.noreply.github.com>
2019-09-16 23:51:32 +0100

    valdation file addded

---
+++ Team goodness/HNG-TEAM-GOODNESS/index.js
@@ -0,0 +1,15 @@
+function myfunction(){
+    let username=document.forms.myform.username.value;
+    let password=document.forms.myform.password.value;
+    let email=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if(username=="" || email.test(username)){
+        alert("Incorrect email or username")
+        event.preventDefault();
+        return false
+    }
+    if(password==""){
+        event.preventDefault();
+        alert('Incorrect password');
+    }
+     
+}
