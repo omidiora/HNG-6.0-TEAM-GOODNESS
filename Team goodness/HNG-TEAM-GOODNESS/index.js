68cd1b1d6dbaa18a361ff84be4a5845b87d6831e
Omidiora Emmanuel <45630269+omidiora@users.noreply.github.com>
2019-09-16 23:53:32 +0100

    file added

--- Team goodness/HNG-TEAM-GOODNESS/index.js
+++ Team goodness/HNG-TEAM-GOODNESS/index.js
@@ -2,7 +2,7 @@ function myfunction(){
     let username=document.forms.myform.username.value;
     let password=document.forms.myform.password.value;
     let email=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    if(username=="" || email.test(username)){
+    if(username=="" || !email.test(username)){
         alert("Incorrect email or username")
         event.preventDefault();
         return false
