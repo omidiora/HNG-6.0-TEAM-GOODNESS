7846bebe0cfb0f64cf5860ffd9dd0ccaccb26339
Happiness <49883808+Tana00@users.noreply.github.com>
2019-09-17 22:40:13 +0100

    Login and Signup pages with bootstrap

---
+++ HNG-6.0-TEAM-GOODNESS/index.php
@@ -0,0 +1,37 @@
+<?php
+
+if(isset($_POST['submit'])){
+    $username=$_POST['username'];
+    $password=$_POST['password'];
+    if($username==" " || $password==" "){
+       return false;
+    }
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
