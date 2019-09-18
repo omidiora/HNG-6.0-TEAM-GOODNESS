5e296c77c85a988751e116542cdd44c566c45966
Omidiora Emmanuel <45630269+omidiora@users.noreply.github.com>
2019-09-18 12:55:13 +0100

    valdiation

--- home.php
+++ home.php
@@ -3,8 +3,8 @@
 if(isset($_POST['submit'])){
     $username=$_POST['username'];
     $password=$_POST['password'];
-    if(!$username==" " || !$password==" "){
-       return false;
+    if($username==" " || !$password==" "){
+       header("location:index.html");
     }
    
 }
