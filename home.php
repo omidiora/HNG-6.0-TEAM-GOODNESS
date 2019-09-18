0de606a9a67e7445bd4c0d95a6e940ce1753e9c9
Omidiora Emmanuel <45630269+omidiora@users.noreply.github.com>
2019-09-18 13:14:08 +0100

    validation

--- home.php
+++ home.php
@@ -3,7 +3,7 @@
 if(isset($_POST['submit'])){
     $username=$_POST['username'];
     $password=$_POST['password'];
-    if($username==" " || !$password==" "){
+    if($username==" " || $password==" "){
        header("location:index.html");
     }
    
